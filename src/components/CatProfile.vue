823eaf742837b95cf8484438aa3f54bdcad95718
ciemna_noc <agarkowa@gmail.com>
2020-11-21 13:07:39 +0100

    form without validation

--- src/components/CatProfile.vue
+++ src/components/CatProfile.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="user-profile_wrapper_post">
         <div class="user-profile" v-bind:style="styleObject">
             <div class="user-profile__user-panel">
                 <h1 class="user-profile__username">@{{ user.userName }}  {{ fullName }}</h1>
@@ -8,19 +9,55 @@
                 </div>
             </div>
         </div>
-        <div class="user-profile__catweets_wrapper">
-            <div class="user-profile_catweet" v-bind:key="catweet.id"  v-for="catweet in user.catweets">
-                {{ catweet.content }}
-            </div>
+    <Catweet :catweets="this.catweets" @favorite="toogleFavorite"/>
+
+    </div>
+    <form class="user-profile__create_catweet" @submit.prevent="createNewCatweet">
+        <label for="newCatweet">
+            <strong> Say yours meow</strong>
+        </label>
+            <textarea id="newCatweet" rows="4" v-model.trim="newCatweetContent" name="newCatweet"/>
+        <div class="user-profile__create_catweet_type">
+            <label for="newCatweetType">
+                <strong>Type: </strong>
+            </label>
+            <select id="newCatweetType" v-model="selectedCatweetType" name="selectedCatweetType">
+                <option :value="option.value" :key="index" v-for="(option, index) in catweetTypes">
+                    {{ option.value }}
+                </option>
+            </select>
+            <label for="catweetAuthor">
+                <strong> Stay in touch </strong>
+            </label>
+            <input id="catweetAuthor" name="catweetAuthor" v-model="catweetAuthor"/>
+
         </div>
+        <div class="user-profile__catweets_wrapper_submit">
+            <input type="submit" value="meow"/>
+        </div>
+
+    </form>
 </template>
 
 <script>
+    import Catweet from "./Catweet";
+
+    Catweet
     export default {
         name: "CatProfile",
+        components : {
+            Catweet,
+        },
         data() {
             return ({
                 followers: 0,
+                newCatweetContent : '',
+                selectedCatweetType : 'instant',
+                catweetAuthor : '@white-cat',
+                catweetTypes : [
+                    { value : 'draft', name : 'draft'},
+                    { value : 'instant', name : 'instant' }
+                ],
                 user: {
                     id: 1,
                     userName: 'marsik-darsik',
@@ -28,13 +65,14 @@
                     lastName: "Sergeeva",
                     email: "google@google.com",
                     isAdmin: true,
-                    catweets: [
-                        {id: 1, content: 'catweets is amazing'},
-                        {id: 2, content: 'meow'},
-                        {id: 3, content: 'Dont meow in my catweets'},
-                        {id: 3, content: ' I/m hungry'}
-                    ]
+
                 },
+                catweets: [
+                    {id: 1, content: 'catweets is amazing', author : "@alice_homepussycat"},
+                    {id: 2, content: 'meow', author : '@bubble_whichsaysmeow'},
+                    {id: 3, content: 'Dont meow in my catweets', author : '@blinded_tom'},
+                    {id: 3, content: ' I/m hungry', author : '@stray'},
+                ],
                 styleObject: {
                     margin: '50px 0 0 50px',
                     paddingLeft: '40px',
@@ -60,9 +98,25 @@
                 }
             },
             methods: {
-                followsUser() {
+                followsUser (){
                     this.followers++
-                }
+                },
+                toogleFavorite(id){
+                    console.log(`Favorite catweets ${id}`)
+                },
+                createNewCatweet (e){
+                    e.preventDefault();
+                    console.log(this.newCatweetContent, this.selectedCatweetType, this.catweetAuthor)
+                    if(this.newCatweetContent && this.selectedCatweetType !== 'draft'){
+                        this.catweets.unshift({
+                            id : this.catweets.length + 1,
+                            content : this.newCatweetContent,
+                            author : this.catweetAuthor,
+                        })
+                    }
+
+                },
+
             },
             mounted() {
                 this.followsUser()
@@ -85,10 +139,43 @@
         flex-direction: column;
         background-color: beige;
         justify-content: center;
-        width : 350px;
+
         min-height: 500px;
         text-align: center;
         margin : 50px 0 0 50px;
         box-shadow: 10px 4px 21px 3px rgba(184,170,184,1);
+    }
+    .user-profile_wrapper_post{
+        display: inline-flex;
+    }
+    .user-profile__create_catweet{
+        flex-direction: column;
+        background-color: antiquewhite;
+        height : max-content;
+        margin-left: 440px;
+        margin-top: 20px;
+        width : 400px;
+        padding : 20px;
+    }
+    .user-profile__create_catweet label{
+        display: block;
+    }
+    .user-profile__create_catweet textarea{
+       margin: 10px auto 10px 10px;
+        width : 360px;
+    }
+    #newCatweetType{
+        width : 100px;
+        height: 30px;
+        font-size: 16px;
+        margin-left: 10px;
+        margin-bottom: 50px;
+    }
+    .user-profile__catweets_wrapper_submit button{
+        margin : 20px auto 20px auto;
+        padding : 5px;
+        width : 100px;
+        background-color: khaki;
+        border-radius: 5px;
     }
 </style>
---
+++ src/components/Catweet.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <div class="user-profile_catweets-wrapper" >
+
+        <div class="user-profile_catweet"  v-bind:key="index"  v-for="(catweet, index) in catweets" @click="favoriteCatweet(catweet.id)">
+            <div class="user-profile_catweet_author">
+                {{ catweet.author }}
+            </div>
+            {{ catweet.content }}
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: "Catweet",
+        props: {
+            catweets : Array,
+            user : Object,
+        },
+        data(){
+            console.log(this.props)
+        },
+        mounted() {
+            console.log(this.props)
+        },
+        methods : {
+            favoriteCatweet(id){
+                this.$emit('favorite', id);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .user-profile_catweets-wrapper{
+        width: 440px;
+        min-height: 100px;
+        margin-left: 50px;
+        margin-top: 50px;
+        background-color: bisque;
+        box-shadow: 10px 4px 21px 3px rgba(184,170,184,1);
+        transition: background-color 1s linear;
+    }
+    .user-profile_catweets-wrapper:hover{
+        background-color: #f0ffff;
+    }
+    .user-profile_catweet{
+        margin:  50px;
+        justify-content: flex-start;
+        flex-direction: column;
+        text-align: start;
+
+    }
+    .user-profile_catweet_author{
+        padding : 3px;
+        border-radius: 5px;
+    }
+    .user-profile_catweet_author:hover{
+        animation: pulsing 2s infinite;
+    }
+
+    @keyframes pulsing {
+        0% {
+            -webkit-transform: scale(0.5, 0.5);
+            transform: scale(0.5, 0.5)
+        }
+        50% {
+            -webkit-transform: scale(1.0, 1.0);
+            transform: scale(1.0, 1.0);
+        }
+        100% {
+            -webkit-transform: scale(0.5, 0.5);
+            transform: scale(0.5, 0.5);
+        }
+    }
+
+</style>
